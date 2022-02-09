1ab0e899c9df1592ab4cee777fc9c3561124df19
neves <bm>
2022-02-09 10:45:24 +0800

    补充部分样式，功能

--- js/bilibili.js
+++ js/bilibili.js
@@ -98,3 +98,34 @@ function createdLi(){
   document.querySelector('.nav-main-center-ul').innerHTML = str
 }
 createdLi()
+
+
+// bili-live-tabs 类名的点击切换
+/**
+ * bili-live-switch我设为display:none;了，所以先默认显示第一个，b站是第三个。根据自己实际情况来
+ */
+let bili_live_tabs_index = 0
+let bili_live_switch = document.querySelectorAll('.bili-live-switch')
+bili_live_switch[0].style.display = 'block'
+// 遍历添加点击事件
+let bili_live_tabs_li = document.querySelectorAll('.bili-live-tabs-li')
+bili_live_tabs_li.forEach((item)=>{
+  item.onclick = (e)=>{
+    if(bili_live_tabs_index == Number(e.target.getAttribute('data-tabs'))){
+      //如果点击的是同一个 不执行后面的代码。
+      return
+    }
+    // 保存 index
+    bili_live_tabs_index = Number(e.target.getAttribute('data-tabs'))
+    // 通过设置display属性显示隐藏响应的模块
+    bili_live_switch.forEach(item =>{
+      item.style.display = 'none'
+    })
+    bili_live_switch[bili_live_tabs_index].style.display = 'block'
+    // 删除/添加 对应位置的active标签
+    bili_live_tabs_li.forEach(item =>{
+      item.classList.remove('active')
+    })
+    bili_live_tabs_li[bili_live_tabs_index].classList.add('active')
+  }
+})
